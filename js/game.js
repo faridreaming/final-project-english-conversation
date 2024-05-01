80198da1acb4dfc837438c09ee8710116bc0ef00
faridrawing <muhammad.farid.yamin@gmail.com>
2024-05-01 23:41:54 +0700

    fixing padding, adding toast and popup

--- js/game.js
+++ js/game.js
@@ -1,15 +1,61 @@
 const playerName = localStorage.getItem("playerName");
 if (!playerName) window.location.href = "../";
 
+// Display Player Name
 const playerNameSpan = document.querySelector(".player-name-span");
 playerNameSpan.textContent = playerName;
 
+// Options
 const header = document.querySelector("header");
 const optionsWrapper = document.querySelector(".options-wrapper");
 const options = document.querySelector(".options");
-
 const menuButton = document.querySelector(".hamburger-menu-button");
-menuButton.addEventListener("click", () => {
+menuButton.addEventListener("click", toggleMenu);
+
+// Change Name
+const popupOverlay = document.querySelector(".popup-overlay");
+const popup = popupOverlay.querySelector(".popup");
+const yesButton = popup.querySelector(".yes");
+const noButton = popup.querySelector(".no");
+const changeNameButton = document.querySelector(".change-name-button");
+changeNameButton.addEventListener("click", showPopup);
+noButton.addEventListener("click", () => {
+  closePopup();
+  showToast("Name change cancelled");
+});
+
+// Functions
+function showPopup() {
+  popupOverlay.style.display = "flex";
+  setTimeout(() => {
+    popupOverlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    setTimeout(() => {
+      popup.style.opacity = 1;
+    }, 50);
+    popup.style.transform = "scale(1)";
+  }, 125);
+}
+
+function closePopup() {
+  setTimeout(() => {
+    setTimeout(() => {
+      popupOverlay.style.display = "none";
+    }, 125);
+    popupOverlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  }, 125);
+  setTimeout(() => {
+    popup.style.transform = "scale(0)";
+  }, 25);
+  popup.style.opacity = 0;
+}
+
+let canClick = true;
+function toggleMenu() {
+  if (!canClick) return;
+  canClick = false;
+  setTimeout(() => {
+    canClick = true;
+  }, 500);
   if (!menuButton.classList.contains("opened")) {
     menuButton.classList.toggle("opened");
     menuButton.textContent = "close";
@@ -19,10 +65,11 @@ menuButton.addEventListener("click", () => {
     menuButton.textContent = "menu";
     closeOptions();
   }
-});
+}
 
 function showOptions() {
-  optionsWrapper.style.padding = "1rem";
+  optionsWrapper.style.padding = "0 1rem 1rem";
+  optionsWrapper.style.height = "fit-content";
   options.style.height = "100%";
   options.style.padding = "1rem";
   setTimeout(() => {
@@ -34,7 +81,18 @@ function closeOptions() {
   options.style.height = "0";
   options.style.opacity = "0";
   setTimeout(() => {
-    options.style.padding = "0";
     optionsWrapper.style.padding = "0";
+    options.style.padding = "0";
   }, 125);
+}
+
+let toastTimeout;
+function showToast(message) {
+  const toast = document.getElementById("toast");
+  toast.innerText = message;
+  toast.classList.add("show");
+  if (toastTimeout) clearTimeout(toastTimeout);
+  toastTimeout = setTimeout(() => {
+    toast.classList.remove("show");
+  }, 1500);
 }

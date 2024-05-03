b639893bdcc28ec39ce4b98e68daf04f8b6087a8
faridrawing <muhammad.farid.yamin@gmail.com>
2024-05-03 16:49:40 +0700

    fix change name fuction, create restart progress button

--- js/game.js
+++ js/game.js
@@ -13,55 +13,83 @@ const menuButton = document.querySelector(".hamburger-menu-button");
 menuButton.addEventListener("click", toggleMenu);
 
 // Change Name
-const popupOverlay = document.querySelector(".popup-overlay");
-const popup = popupOverlay.querySelector(".popup");
 const changeNameButton = document.querySelector(".change-name-button");
-const changeNameForm = document.querySelector(".change-name");
-const newNameInput = changeNameForm.querySelector("input");
-const noButton = popup.querySelector(".no");
 const resetProgressButton = document.querySelector(".restart-progress-button");
 
-changeNameButton.addEventListener("click", showPopup);
-newNameInput.value = playerName;
-
-changeNameForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  changeName();
-});
+changeNameButton.addEventListener("click", handleChangeName);
+function handleChangeName() {
+  const newNameInput = `<input type="text" placeholder="Your name" spellcheck="false" value="${playerName}">`;
+  showPopup("change-name", "Please enter your new name", newNameInput, [
+    "Cancel",
+    "Confirm",
+  ]);
+  const changeNameForm = document.querySelector(".popup-form.change-name");
+  changeNameForm.addEventListener("submit", handleChangeNameSubmit);
+  changeNameForm.addEventListener("reset", (event) => {
+    event.preventDefault();
+    closePopup();
+    showToast("Name change cancelled", "fail");
+    setTimeout(() => {
+      newNameInput.value = playerName;
+    }, 125);
+  });
+}
 
 resetProgressButton.addEventListener("click", restartProgress);
-
-noButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  closePopup();
-  showToast("Name change cancelled", "fail");
-  setTimeout(() => {
-    newNameInput.value = playerName;
-  }, 125);
-});
-
 // Functions
 function restartProgress() {
-  localStorage.removeItem("playerName");
-  window.location.href = "../";
+  showPopup(
+    "restart-progress",
+    "Are you sure you want to restart your progress?",
+    "",
+    ["No", "Yes"]
+  );
+  const resetProgressForm = document.querySelector(
+    ".popup-form.restart-progress"
+  );
+  resetProgressForm.addEventListener("submit", () => {
+    localStorage.removeItem("playerName");
+    window.location.href = "../";
+  });
+  resetProgressForm.addEventListener("reset", closePopup);
 }
 
 function changeName() {
-  const newName = newNameInput.value;
-  if (newName === playerName) {
+  const newName = document.querySelector(".popup-form.change-name input").value;
+  if (!newName || newName === playerName) {
     showToast("Name change cancelled", "fail");
     closePopup();
     return;
   }
-
   playerName = newName;
-  localStorage.setItem("playerName", playerName);
   playerNameSpan.textContent = playerName;
+  console.log(`${playerName} success changed to ${newName}`);
   showToast("Name changed successfully", "success");
   closePopup();
 }
 
-function showPopup() {
+function handleChangeNameSubmit(event) {
+  event.preventDefault();
+  changeName();
+  const changeNameForm = document.querySelector(".popup-form.change-name");
+  changeNameForm.removeEventListener("submit", handleChangeNameSubmit);
+}
+
+// Popups
+const popupOverlay = document.querySelector(".popup-overlay");
+const popup = popupOverlay.querySelector(".popup");
+const popupForm = popup.querySelector(".popup-form");
+const popupMessage = popup.querySelector(".popup-message");
+const popupElement = popup.querySelector(".popup-element");
+const noButton = popup.querySelector(".no");
+const yesButton = popup.querySelector(".yes");
+
+function showPopup(popUpName, message, element, buttonText) {
+  popupForm.classList.add(popUpName);
+  popupMessage.textContent = message;
+  popupElement.innerHTML = element;
+  noButton.textContent = buttonText[0];
+  yesButton.textContent = buttonText[1];
   popupOverlay.style.display = "flex";
   setTimeout(() => {
     popupOverlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
@@ -70,7 +98,6 @@ function showPopup() {
     }, 50);
     popup.style.transform = "scale(1)";
   }, 125);
-  newNameInput.focus();
 }
 
 function closePopup() {
